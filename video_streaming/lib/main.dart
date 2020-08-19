7ffb862f70e0589a8db85106d89ac7b85f13c343
Prat15072000 <pratsimple2000@gmail.com>
2020-08-19 21:20:59 +0530

    initial push

--- video_streaming/lib/main.dart
+++ video_streaming/lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import './src/pages/index.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: IndexPage(),
+    );
   }
 }
---
+++ video_streaming/lib/src/utils/settings.dart
@@ -0,0 +1,2 @@
+// Agora AppId
+const APP_ID = '1458a00f60e6405996b4dc5c98554736';
